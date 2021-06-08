346a9852f0561cbe3519cfafeed9a16e60b62334
NamNh <nam.nh@agiletech.vn>
2021-06-08 22:48:49 +0700

    umi3

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -1,36 +1,11 @@
-import { IConfig } from 'umi-types';
+import { defineConfig } from 'umi';
 
-// ref: https://umijs.org/config/
-const config: IConfig =  {
-  treeShaking: true,
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
   routes: [
-    {
-      path: '/',
-      component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    },
-    {
-      component: '../pages/index'
-    }
+    { path: '/', component: '@/pages/index' },
   ],
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: false,
-      dynamicImport: { webpackChunkName: true },
-      title: 'frontend',
-      dll: true,
-
-      routes: {
-        exclude: [
-          /components\//,
-        ],
-      },
-    }],
-  ],
-}
-
-export default config;
+  fastRefresh: {},
+});
